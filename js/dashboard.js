354f4bebf8105edf513be0f37556b02491af7deb
Derian Pinto <31827066+pintoderian@users.noreply.github.com>
2017-09-12 11:26:34 -0500

    Añadiendo creditos

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,8 @@
+/*!
+ * Simple Dashboard Bootstrap 4 (https://github.com/pintoderian/dashboard-bootstrap-4)
+ * Copyright 2017 Codegea
+ * Licensed under MIT (https://github.com/pintoderian/dashboard-bootstrap-4/blob/master/LICENSE)
+ */
 (function($) {
   "use strict"; 
   $('[data-toggle="tooltip"]').tooltip();
@@ -5,7 +10,7 @@
         var topOffset = 33;
         var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
-            topOffset = 100; // 2-row-menu
+            topOffset = 100;
         }
         $('div.navbar-collapse').removeClass('collapse');
         var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
